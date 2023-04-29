74f5102a33cbcffebc91660a4e655e175890d744
rendivy <arlosaneq@gmail.com>
2023-04-29 13:15:53 +0700

    Added BottomNavGraph and style BottomBar

---
+++ app/src/main/java/com/example/android_blueprint/BottomScreen.kt
@@ -0,0 +1,110 @@
+package com.example.android_blueprint.view
+
+import android.annotation.SuppressLint
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.RowScope
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.BottomNavigation
+import androidx.compose.material.Icon
+import androidx.compose.material.Scaffold
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavDestination
+import androidx.navigation.NavDestination.Companion.hierarchy
+import androidx.navigation.NavGraph.Companion.findStartDestination
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
+
+@SuppressLint("UnusedMaterialScaffoldPaddingParameter")
+@Preview(showBackground = true)
+@Composable
+fun MainScreen() {
+    val navController = rememberNavController()
+    Scaffold(
+        bottomBar = {
+            BottomBar(
+                modifier = Modifier.background(Color.White),
+                navController = navController
+            )
+        },
+    )
+    {
+        BottomNavGraph(navController = navController)
+    }
+}
+
+
+@Composable
+fun BottomBar(modifier: Modifier, navController: NavHostController) {
+    val screens = listOf(
+        BottomBarScreen.Field,
+        BottomBarScreen.Console,
+        BottomBarScreen.BlockOfList,
+    )
+    val navBackStackEntry by navController.currentBackStackEntryAsState()
+    val currentDestination = navBackStackEntry?.destination
+
+    BottomNavigation(backgroundColor = Color.White) {
+        screens.forEach() { screen ->
+            AddItem(
+                screen = screen,
+                currentDestination = currentDestination,
+                navController = navController
+            )
+        }
+    }
+}
+
+
+@Composable
+fun RowScope.AddItem(
+    screen: BottomBarScreen,
+    currentDestination: NavDestination?,
+    navController: NavHostController
+) {
+    val selected = currentDestination?.hierarchy?.any { it.route == screen.route } == true
+    val background = if (selected) Color.Yellow else Color.Transparent
+    Box(
+        modifier = Modifier
+            .clip(CircleShape)
+            .height(40.dp)
+            .background(background)
+            .clickable(onClick = {
+                navController.navigate(screen.route) {
+                    popUpTo(navController.graph.findStartDestination().id)
+                    launchSingleTop = true
+                }
+            })
+    )
+    {
+        Row(
+            modifier = Modifier
+                .padding(start = 10.dp, end = 10.dp, top = 8.dp, bottom = 8.dp),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.spacedBy(4.dp),
+        ) {
+            Icon(imageVector = screen.icon, contentDescription = null, tint = Color.Black)
+            AnimatedVisibility(visible = selected) {
+                Text(
+                    text = screen.title,
+                    color = Color.Black
+                )
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/example/android_blueprint/view/BottomBarScreen.kt
@@ -0,0 +1,90 @@
+package com.example.android_blueprint.view
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountCircle
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Settings
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+sealed class BottomBarScreen(
+    val route: String,
+    val title: String,
+    val icon: ImageVector
+) {
+    object Field : BottomBarScreen(
+        route = "Field",
+        title = "Field",
+        icon = Icons.Default.Home
+    )
+
+    object BlockOfList : BottomBarScreen(
+        route = "BlockList",
+        title = "Block list",
+        icon = Icons.Default.AccountCircle
+    )
+
+    object Console : BottomBarScreen(
+        route = "Console",
+        title = "Console",
+        icon = Icons.Default.Settings
+    )
+
+}
+
+
+@Composable
+fun FieldScreen() {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.White).padding(8.dp),
+        contentAlignment = Alignment.Center
+    ) {
+        Text(text = "Здесь наше полотно с блоками", fontSize = 24.sp, fontWeight = FontWeight.Bold)
+    }
+}
+
+@Composable
+fun ListScreen() {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.White).padding(8.dp),
+        contentAlignment = Alignment.Center
+    ) {
+        Text(
+            text = "Здесь будет список с нашими блоками",
+            fontSize = 24.sp,
+            fontWeight = FontWeight.Bold
+        )
+    }
+}
+
+
+@Composable
+fun ConsoleScreen() {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.White).padding(8.dp),
+        contentAlignment = Alignment.Center
+    ) {
+        Text(
+            text = "Здесь будет наша консоль с выводом, планирую сюда перекидывать пользователя, либо по клику, либо по нажанию кнопки компиляции на главном ",
+            fontSize = 24.sp,
+            fontWeight = FontWeight.Bold
+        )
+    }
+}
