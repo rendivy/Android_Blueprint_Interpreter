7b912f59483691f369c96fbb07a1c42edd785e29
Maksim <mazhur04@gmail.com>
2023-05-01 21:43:03 +0700

    Added some sizes

---
+++ app/src/main/java/com/example/android_blueprint/ui/theme/Padding.kt
@@ -0,0 +1,6 @@
+package com.example.android_blueprint.ui.theme
+
+import androidx.compose.ui.unit.dp
+
+val BlockListPadding = 16.dp
+val BottomBarPadding = 56.dp
---
+++ app/src/main/java/com/example/android_blueprint/ui/theme/Shape.kt
@@ -0,0 +1,6 @@
+package com.example.android_blueprint.ui.theme
+
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.ui.unit.dp
+
+val BlockShape = RoundedCornerShape(10.dp)
