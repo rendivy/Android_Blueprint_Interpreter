df6c7bef8fe1e758de13a073655d07609eac4f76
Maksim <mazhur04@gmail.com>
2023-05-02 21:09:16 +0700

    Added block class

---
+++ app/src/main/java/com/example/android_blueprint/model/Blocks.kt
@@ -0,0 +1,10 @@
+package com.example.android_blueprint.model
+
+enum class Blocks(val type: String) {
+    SUBTRACTION("-"),
+    MULTIPLICATION("×"),
+    DIVISION("÷"),
+    REMAINDER("%"),
+    ADDITION("+"),
+    EQUALITY("=")
+}
